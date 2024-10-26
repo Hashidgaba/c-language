39753faccb452277b88f6f89ce02d4ab088a9501
Hashidgaba <hashidgaba@gmail.com>
2024-10-26 14:52:55 +0500

    all changes

---
+++ second task/task2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string student;
+
+    cout << "Enter your name: ";
+    cin >> student; 
+
+    int Maths ;
+    float English ;
+    int urdu;
+    
+    cout << "enter your maths marks: " << endl;
+    cin >>Maths;
+
+    cout << "Enter your english marks; " << endl;
+    cin >> English;
+
+    cout << "Enter your urdu marks: " <<endl;
+    cin >> urdu;
+
+
+    cout << "Here your marks is: "<< endl << "Maths = "<< Maths << endl << "English = " << English << endl << "Urdu = " << urdu << endl;
+    int avg = Maths + English + urdu;
+    cout << "average will be: " << endl << avg/3;
+
+
+    return 0;
+    
+}
