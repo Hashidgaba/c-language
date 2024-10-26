4c87d15d331210694873f78fa39174a360ac9a45
Hashidgaba <hashidgaba@gmail.com>
2024-10-26 15:17:26 +0500

    all changes

---
+++ Lab_Tasks/Assignment_01/task.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age;
+    string name;
+    cout << "Enter your Name: " << endl;
+    cin >> name;
+    cout << "Enter your age: " << endl;
+    cin >> age;
+    cout << name << " welcome in this tech world" ;
+    return 0;
+
+}
