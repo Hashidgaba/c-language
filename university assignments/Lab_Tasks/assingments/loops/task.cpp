b11fdc3adb1fa09889523250df15acc2612a98f4
Hashidgaba <hashidgaba@gmail.com>
2024-11-04 19:28:51 +0500

    all changes

---
+++ university assignments/Lab_Tasks/assingments/loops/task.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //create a program that prints the numebr from 1 to 10 using for loop
+    for (int i = 1; i <= 10 ; i++){
+        cout << "number is: = " << i << endl;
+    }
+    return 0;
+
+}
