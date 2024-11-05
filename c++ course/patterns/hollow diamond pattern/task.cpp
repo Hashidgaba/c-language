9c9b452a65f20918315287bf9e0de2bcbba93efa
Hashidgaba <hashidgaba@gmail.com>
2024-11-05 20:47:15 +0500

    All updates done

--- c++ course/patterns/hollow diamond pattern/task.cpp
+++ c++ course/patterns/hollow diamond pattern/task.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 int main(){
-    int n = 4;
+    int n;
+    cout << "enter any number: " << endl;
+    cin >> n;
+
     for (int i = 0; i < n ; i++){
         //spaces
         for (int j = 0; j < n - i -1; j++){
@@ -19,7 +22,7 @@ int main(){
 
         cout << endl;
     }
-
+    //bottom
     for (int i = 0 ; i < n - 1 ; i++){
         //spaces
         for (int j = 0; j < i + 1; j++){
@@ -27,10 +30,15 @@ int main(){
         }
         cout << "*";
         //sapces
-        for (int j = 2 * (n - 2 - i)-1 ; j > 0 ; j-- ){
+        
+        if ( n - i - 1 > 1){
+            for (int j = 2 * (n - 2 - i)-1 ; j > 0 ; j-- ){
             cout << " ";
         }
-        if (i != n - 1) cout << "*";
+        cout << "*";
+        }
+        // if (n - i -1 > 1)cout << "*";
+        
 
         cout << endl;
     }
--- c++ course/patterns/pyramid pattern/task.cpp
+++ c++ course/patterns/pyramid pattern/task.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n = 10;
+    int n = 4;
     for (int i = 0; i < n ; i++){   
         //spaces
         for (int j = 0 ; j < n - i - 1; j++){
