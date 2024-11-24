b976e95c9f07797f458fc409e7c944332b1be8a1
Hashidgaba <hashidgaba@gmail.com>
2024-11-24 17:15:21 +0500

    all changes

--- university assignments/theory_task/Before midterm tasks/fourth_question/task.cpp
+++ university assignments/theory_task/Before midterm tasks/fourth_question/task.cpp
@@ -1,21 +1,41 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-int main(){
-    //solve the quadratic eq
-    double a, b , c, disciminant;
-    float x1, x2;
-    cout << "enter coefficient of a" << endl;
-    cin >> a;
-    cout << "enter coefficien of b" << endl;
-    cin >> b;
-    cout << "enter coefficient of c" << endl;
-    cin>> c;
-    disciminant = b * b -4 * a * c;
-    x1 = (-b + sqrt(disciminant))/ (2 * a);
-    x2 = (-b - sqrt(disciminant)) / (2 * a);
-    cout << "value of x1 is = " << x1 << endl;
-    cout << "value of x2 is = " << x2 << endl;
-    return 0;
+int main()
+{
 
+    // solve the quadratic equation
+    double a, b, c, discriminant;
+    double x1, x2;
+    cout << "enter coefficient of a, b, c" << endl;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    discriminant = pow(b, 2) - 4 * a * c;
+    // if discriminant is become negative
+    if (discriminant < 0)
+    {
+        x1 = (-b + sqrt(-discriminant)) / (2 * a);
+        x2 = (-b - sqrt(-discriminant)) / (2 * a);
+        cout << "value of x1 is = " << x1 << "i" << endl;
+        cout << "value of x2 is = " << x2 << "i" << endl;
+    }
+    //when discriminant is become zero
+    else if (discriminant == 0)
+    {
+        x1 = (-b) / (2 * a);
+        x2 = (-b) / (2 * a);
+        cout << "value of x1 is = " << x1 << endl;
+        cout << "value of x2 is = " << x2 << endl;
+    }
+    //when discriminant is become positive
+    else
+    {
+        x1 = (-b + sqrt(discriminant)) / (2 * a);
+        x2 = (-b - sqrt(discriminant)) / (2 * a);
+        cout << "value of x1 is = " << x1 << endl;
+        cout << "value of x2 is = " << x2 << endl;
+    }
+
+    return 0;
 }
--- university assignments/theory_task/Before midterm tasks/tenth_question/task.cpp
+++ university assignments/theory_task/Before midterm tasks/tenth_question/task.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
-int main(){
-    cout << "welcome to my page" << endl;
+int main()
+{
+
+    // solve the quadratic equation
+    cout << "---------------solve quadratic equation--------------" << endl;
+    double a, b, c, discriminant;
+    double x1, x2;
+    cout << "enter coefficient of a, b, c" << endl;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    discriminant = pow(b, 2) - 4 * a * c;
+    // if discriminant is become negative
+    if (discriminant < 0)
+    {
+        x1 = (-b + sqrt(-discriminant)) / (2 * a);
+        x2 = (-b - sqrt(-discriminant)) / (2 * a);
+        cout << "value of x1 is = " << x1 << "i" << endl;
+        cout << "value of x2 is = " << x2 << "i" << endl;
+    }
+    //when discriminant is become zero
+    else if (discriminant == 0)
+    {
+        x1 = (-b) / (2 * a);
+        x2 = (-b) / (2 * a);
+        cout << "value of x1 is = " << x1 << endl;
+        cout << "value of x2 is = " << x2 << endl;
+    }
+    //when discriminant is become positive
+    else
+    {
+        x1 = (-b + sqrt(discriminant)) / (2 * a);
+        x2 = (-b - sqrt(discriminant)) / (2 * a);
+        cout << "value of x1 is = " << x1 << endl;
+        cout << "value of x2 is = " << x2 << endl;
+    }
+
     return 0;
-
-
 }
