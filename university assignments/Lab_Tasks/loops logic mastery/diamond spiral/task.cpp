28bfb9900cd25d27093e49f0c80c9b60eaff1fad
Hashidgaba <hashidgaba@gmail.com>
2024-11-25 16:50:56 +0500

    done

--- university assignments/Lab_Tasks/loops logic mastery/diamond spiral/task.cpp
+++ university assignments/Lab_Tasks/loops logic mastery/diamond spiral/task.cpp
@@ -2,57 +2,6 @@
 using namespace std;
 
 int main(){
-    // int n = 5;
-    // int number = 1;
-    // //upper side 
-    // for (int i = 0; i< n; i++){
-    //     //spaces
-    //     for (int j = 0; j < n - i; j++){
-    //         cout << " ";
-    //     }
-    //     //number
-    //     cout << number;
-    //     number ++;
-    //     //spaces
-    //     for (int j = 0; j < 2 * i - 1; j++){
-    //         cout << " ";
-    //     }
-    //     //number
-    //     if(i != 0){
-    //         cout << number;
-    //         number ++;
-    //     }
-    //     cout << endl;
-    // }
-    // //bottom side
-    
-    // for (int i = 1; i <= n - 1; i++){
-    //     //spaces
-    //     for (int j = 1; j <= i; j++ ){
-    //         cout << " ";
-    //     }
-    //     //number 
-    //     if (i == 1){
-    //         cout << number - 4;
-    //         number -= 4;
-    //     }else if(i == n - 1){
-    //         cout << number - 1;
-    //     }
-    //     else{
-    //         number -= 2;
-    //         cout << number;
-    //     }
-    //     //spaces
-    //     for (int j =  2 * i - 1 ; j <= n + 2 ; j++){
-    //         if(i != n - 1){
-    //             cout << "";
-    //         }else{
-    //             continue;
-    //         }
-    //     }
-
-    //     cout << endl;
-    // }
     int n = 5;
     int evenNo = 0;
     int oddNo = 1;
