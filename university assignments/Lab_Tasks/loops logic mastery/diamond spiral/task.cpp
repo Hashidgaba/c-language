87632568dbac4d2d49b0cc3109823118b33bbb0f
Hashidgaba <hashidgaba@gmail.com>
2024-11-25 16:44:50 +0500

    all changes

--- university assignments/Lab_Tasks/loops logic mastery/diamond spiral/task.cpp
+++ university assignments/Lab_Tasks/loops logic mastery/diamond spiral/task.cpp
@@ -1,60 +1,111 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main(){
+    // int n = 5;
+    // int number = 1;
+    // //upper side 
+    // for (int i = 0; i< n; i++){
+    //     //spaces
+    //     for (int j = 0; j < n - i; j++){
+    //         cout << " ";
+    //     }
+    //     //number
+    //     cout << number;
+    //     number ++;
+    //     //spaces
+    //     for (int j = 0; j < 2 * i - 1; j++){
+    //         cout << " ";
+    //     }
+    //     //number
+    //     if(i != 0){
+    //         cout << number;
+    //         number ++;
+    //     }
+    //     cout << endl;
+    // }
+    // //bottom side
+    
+    // for (int i = 1; i <= n - 1; i++){
+    //     //spaces
+    //     for (int j = 1; j <= i; j++ ){
+    //         cout << " ";
+    //     }
+    //     //number 
+    //     if (i == 1){
+    //         cout << number - 4;
+    //         number -= 4;
+    //     }else if(i == n - 1){
+    //         cout << number - 1;
+    //     }
+    //     else{
+    //         number -= 2;
+    //         cout << number;
+    //     }
+    //     //spaces
+    //     for (int j =  2 * i - 1 ; j <= n + 2 ; j++){
+    //         if(i != n - 1){
+    //             cout << "";
+    //         }else{
+    //             continue;
+    //         }
+    //     }
+
+    //     cout << endl;
+    // }
     int n = 5;
-    int number = 1;
-
-    for (int i = 0; i < n; i++)
-    {
-        // spaces
-        for (int j = 0; j < n - i - 1; j++)
-        {
+    int evenNo = 0;
+    int oddNo = 1;
+    //upper side 
+    for (int i = 0; i< n; i++){
+        //spaces
+        for (int j = i; j < n - 1; j++ ){
             cout << " ";
         }
-        // numebr
-        cout << number;
-        number++;
-
-        // spaces
-        for (int j = 0; j < 2 * i - 1; j++)
-        {
+        //number even
+        if (i == 0){
+            cout << evenNo + 1;
+        }else {
+            evenNo += 2;
+            cout << evenNo;  
+        }
+        //spaces
+        for (int j = 0; j < 2 * i - 1; j++){
             cout << " ";
         }
-        if (i > 0)
-        {
-            cout << number;
-            number++;
+        //numebr odd
+        if(i != 0){
+            oddNo += 2;
+            cout << oddNo;
         }
         cout << endl;
     }
-    number = number - 2;
-    // bottom
-    for (int i = 0; i < n - 1; i++)
-    {
-        // spaces
-        for (int j = 0; j < i + 1; j++)
-        {
+    //bottom
+    for (int i = 1; i <= n - 1; i++){
+        //spaces
+        for (int j = 1; j <= i; j++){
             cout << " ";
         }
-        // number
-        --number;
-        cout << number;
-
-        // sapces
-
-        if (n - i - 1 > 1)
-        {
-            for (int j = 2 * (n - 2 - i) - 1; j > 0; j--)
-            {
-                cout << " ";
-            }
-            --number;
-            cout << number;
+        //number
+        if(i == n - 1){
+            cout << evenNo - 1;
+        }else{
+            evenNo -= 2;
+            cout << evenNo;
         }
-        cout << endl;
-    }
+        //spaces
+        for (int j = 2 * i - 1; j<= n; j++){
+            cout << " ";
+        }
+        //odd number
+        if(i != n - 1){
+            oddNo -= 2;
+            cout << oddNo;
+        }
 
+        cout << endl;
+
+    }
     return 0;
+
 }
