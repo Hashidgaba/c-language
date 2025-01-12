d4bd694eb5c95c2c7f949e01d3192f43add9f0d5
Hashidgaba <hashidgaba@gmail.com>
2025-01-12 20:05:10 +0500

    done product of array

--- c++ questions/singleNumber/task.cpp
+++ c++ questions/singleNumber/task.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int singleNumber(vector <int> nums){
+int singleNumber(vector <int>& nums){
     int ans = 0;
     for (int val : nums){
         ans ^= val;
