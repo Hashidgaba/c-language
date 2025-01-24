b5324232f70c1e2ab4d72e778a19efb6bcdb792b
Hashidgaba <hashidgaba@gmail.com>
2025-01-24 19:01:22 +0500

    done theory question 08 and 09

--- university assignments/theory_task/after midterm tasks/Question 09/task.cpp
+++ university assignments/theory_task/after midterm tasks/Question 09/task.cpp
@@ -2,23 +2,29 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
-   int n = 3;
-   int arr[n + 1] = {1,2,5};
-   arr[n] = 45;
-
-    // Sorting logic in descendig order
-    for (int i = 0; i < n + 1; i++) {
-        for (int j = i + 1; j < n + 1; j++) { 
-            if (arr[i] < arr[j]) {
-                swap(arr[i], arr[j]);
+int main()
+{
+    int n = 3;
+    int arr[n + 1] = {1, 2, 5};
+    arr[n] = 45;
+    for (int i = 0; i < n + 1; i++)
+    {
+        for (int j = i + 1; j < n + 1; j++)
+        {
+            if (arr[i] < arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                
             }
         }
     }
 
     // Sorted array print karna
     cout << "Merged array in descending order is: " << endl;
-    for (int i = 0; i < n + 1; i++) {
+    for (int i = 0; i < n + 1; i++)
+    {
         cout << arr[i] << " ";
     }
 
