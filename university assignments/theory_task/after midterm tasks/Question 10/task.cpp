247a589ba6deb41055c506d7173f06965834a3cb
Hashidgaba <hashidgaba@gmail.com>
2025-01-26 18:26:39 +0500

    done question 10

--- university assignments/theory_task/after midterm tasks/Question 10/task.cpp
+++ university assignments/theory_task/after midterm tasks/Question 10/task.cpp
@@ -3,58 +3,65 @@ using namespace std;
 
 int main()
 {
-    int arr1[2][2];
-    int arr2[2][2];
+    int arr1[2][2] = {{1, 3}, {1, 2}};
+    int arr2[2][2] = {{1, 2}, {1, 2}};
+    int orderArr1 = 0;
+    int orderArr2 = 0;
     bool isEqual = true;
-    cout << "input elements in first array" << endl;
+    cout << "Elements of first matrix: " << endl;
     for (int i = 0; i < 2; i++)
     {
         for (int j = 0; j < 2; j++)
         {
-            cout << "input element: " << endl;
-            cin >> arr1[i][j];
+            cout << arr1[i][j] << " ";
+            orderArr1++;
         }
-    }
 
-    cout << "input elements in second array:" << endl;
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            cout << "input element: " << endl;
-            cin >> arr2[i][j];
-        }
-    }
-    //additio of two matrices
-    cout << "Addition of two matrices" << endl;
-      for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-           cout << arr1[i][j] + arr2[i][j] << " ";
-        }
         cout << endl;
     }
-    //check matrices are equal or not
+
+    cout << "\nElements of second matrix: " << endl;
     for (int i = 0; i < 2; i++)
     {
         for (int j = 0; j < 2; j++)
         {
-            if (arr1[i][j] != arr2[i][j])
-            {
-                isEqual = false;
-            }
+            cout << arr2[i][j] << " ";
+            orderArr2++;
         }
+
+        cout << endl;
     }
 
-    if (isEqual)
+    // if order same then matrix can be add
+    if (orderArr1 == orderArr2)
     {
-        cout << "matrices are equal" << endl;
+        cout << "Addition of two matrices will be: " << endl;
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                cout << arr1[i][j] + arr2[i][j] << " ";
+                if (arr1[i][j] != arr2[i][j])
+                {
+                    isEqual = false;
+                }
+            }
+            cout << endl;
+        }
+        if(isEqual){
+            cout << "Martices are equal" << endl;
+
+        }else{
+            cout << "two matrix are not equal" << endl;
+        }
     }
     else
     {
-        cout << "Matrices are not equal" << endl;
+        cout << "Matrix can not be add becaues: " << endl;
+        cout << "order of first matrix is " << orderArr1 << endl;
+        cout << "order of second matrix will be " << orderArr2 << endl;
+        cout << "matrices are not equal" << endl;
+        
     }
-
     return 0;
 }
