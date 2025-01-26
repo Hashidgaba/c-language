88adb03e3d14e064a470caceec21c8128e5e1a5a
Hashidgaba <hashidgaba@gmail.com>
2025-01-26 21:40:47 +0500

    done question 15

--- university assignments/theory_task/after midterm tasks/Question 15/code.cpp
+++ university assignments/theory_task/after midterm tasks/Question 15/code.cpp
@@ -4,21 +4,21 @@ using namespace std;
 
 int main() {
     string input;
-    int TotalCount[256] = {0}; // Array to store character frequencies
+    int Count[256] = {0}; // Array to store character frequencies
 
     cout << "Enter your String: " << endl;
     getline(cin, input);
 
     // Count occurrences of each character
     for (int i = 0; input[i] != '\0'; i++) {
-        TotalCount[input[i]]++;
+        Count[input[i]]++;
     }
 
     // Display the counts
     cout << "The count of each character is:\n";
     for (int i = 0; i < 256; i++) { // Corrected loop condition
-        if (TotalCount[i] > 0) { // Only print characters that appear in the string
-            cout << (char)i << " " << TotalCount[i] << endl;
+        if (Count[i] > 0) { // Only print characters that appear in the string
+            cout << (char)i << " " << Count[i] << endl;
         }
     }
 
